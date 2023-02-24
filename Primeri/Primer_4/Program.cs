6122d6160feb703da59a8266f1959b6f252b3616
Ivan Pivnev <pivnev-1998@mail.ru>
2023-02-24 21:59:03 +0300

    Sem5

---
+++ Primeri/Primer_4/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 3: Задайте одномерный массив из 10 случайных чисел в диапазоне от 1 до 200. Найдите количество  двузначных элементов массива. 
+// Пример для массива из 5, а не 10 элементов. В своём решении сделайте для 10
+// [5, 18, 123, 6, 2] -> 1
+// [1, 2, 3, 6, 2] -> 0
+// [10, 11, 12, 13, 14] -> 5
+int[] GenerateArray(int length, int minValue, int maxValue)
+{
+    int[] result = new int[length];
+    Random rnd = new Random();
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = rnd.Next(minValue, maxValue + 1);
+    }
+    return result;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write(array[i] + " ");
+    }
+    System.Console.WriteLine();
+}
+
+int SearchTwoDigits(int[] array)
+{
+    int countDigits = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 9 && array[i] < 100) countDigits++;
+    }
+    return countDigits;
+}
+int size = 10;
+int minValue = 1;
+int maxValue = 200;
+int[] myArray = GenerateArray(size, minValue, maxValue);
+PrintArray(myArray);
+System.Console.WriteLine($"Количество двузначных элементов = {SearchTwoDigits(myArray)}");
---
+++ Primeri/Primer_5/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 4: Найдите произведение пар чисел в одномерном массиве. 
+// Парой считаем первый и последний элемент, второй и предпоследний и т.д. Результат запишите в новом массиве.
+// [1 2 3 4 5] -> 5 8 3
+// [6 7 3 6] -> 36 21
+int[] GenerateArray(int length, int minValue, int maxValue) // Метод создание массива со случайными числами. Длина будет Length, Минимальное значение minValue, максимальное значение maxValue
+
+{
+    int[] result = new int[length]; // создания массива 
+    Random rnd = new Random(); //создание генератора случайных чисель 
+    for (int i = 0; i < result.Length; i++) // Цикл элементого перебора массива
+    {
+        result[i] = rnd.Next(minValue, maxValue + 1); // перебор массива, со сохраниеним ячеки массива 
+
+    }
+    return result; //возвращаем массив 
+}
+int[] PairsArray(int[] array)
+{
+    int[] result = new int[array.Length / 2 + array.Length % 2];
+    result [result.Length-1]=array[result.Length-1];
+    for (int i = 0; i < array.Length / 2; i++)
+    {
+        int lastIndex = array.Length-1-i;
+        result[i]=array[i]* array[lastIndex]; 
+        }
+        return result;
+}
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write(array[i] + " ");
+    }
+    System.Console.WriteLine();
+}
+int [] array =GenerateArray(8,1,6);
+PrintArray (array);
+PrintArray (PairsArray(array));
